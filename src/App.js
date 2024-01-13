93ba05b964ca621ef4597e3ec720b6a036d333b9
zhigazh2017@outlook.com <zhigazh2017@outlook.com>
2024-01-13 13:36:34 +0600

    comment

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import "./styles/main.css";
+
+import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
+import Navbar from "./components/nav-bar/Navbar";
+import Footer from "./components/footer/footer";
+import Home from "./pages/home";
+import Projects from "./pages/Projects";
+import Contacts from "./pages/Contacts";
+import Project from "./pages/Project";
+
+import ScrollToTop from "./utils/scrollToTop";
+
+function App() {
+  return (
+    <div className="App">
+        <Router>
+          <ScrollToTop/>
+          <Navbar/>
+          <Routes>
+              <Route path="/" element={<Home/>}/>
+              <Route path="/projects" element={<Projects/>}/> 
+              <Route path="/contacts" element={<Contacts/>}/> 
+              <Route path="/project/:id" element={<Project/>}/> 
+          </Routes>
+          <Footer/>
+        </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/utils/detectDarkMode.js
@@ -0,0 +1,11 @@
+const detectDarkMode = ()=>{
+        // 1. Проверка темной темы на уровне системных настроек
+if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches ) {
+    return 'dark';
+}
+else{
+    return 'light';
+}
+
+}
+export default detectDarkMode;
